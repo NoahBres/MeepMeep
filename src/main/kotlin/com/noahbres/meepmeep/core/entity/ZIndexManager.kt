e8250c0ea6d7402da23e2e16d417c90c37a0e385
Noah Bresler <noahbresler2002@gmail.com>
2020-08-30 13:23:48 -0500

    feat(core): implement a z index manager

---
+++ src/main/kotlin/com/noahbres/meepmeep/core/entity/ZIndexManager.kt
@@ -0,0 +1,39 @@
+package com.noahbres.meepmeep.core.entity
+
+class ZIndexManager {
+    private var hierarchyMap = LinkedHashMap<String, MutableList<Entity>>()
+
+    fun setTagHierarchy(vararg tags: String) {
+        val reversedTags = tags.reversed()
+
+        hierarchyMap["UNKNOWN_TAG_GROUP"] = mutableListOf()
+
+        for (tag in reversedTags) {
+            hierarchyMap[tag] = mutableListOf()
+        }
+    }
+
+    fun addEntity(entity: Entity) {
+        if (hierarchyMap.containsKey(entity.tag)) {
+            hierarchyMap[entity.tag]!!.add(entity)
+        } else {
+            hierarchyMap["UNKNOWN_TAG_GROUP"]!!.add(entity)
+        }
+
+        setIndices()
+    }
+
+    private fun setIndices() {
+        val entityList = mutableListOf<Entity>()
+
+        hierarchyMap.forEach { (_, tagGroup) ->
+            entityList.addAll(tagGroup)
+        }
+
+        entityList.forEachIndexed { index, entity ->
+            println(entity)
+            println(index)
+            entity.zIndex = index
+        }
+    }
+}
